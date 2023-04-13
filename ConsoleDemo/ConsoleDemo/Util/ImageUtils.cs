679d10bbda76685c874a788e7cac618160d44c65
ALI <1416978277@qq.com>
2023-04-13 21:56:48 +0800

    0413

--- ConsoleDemo/ConsoleDemo/Util/ImageUtils.cs
+++ ConsoleDemo/ConsoleDemo/Util/ImageUtils.cs
@@ -1,15 +1,19 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
-using System.Net.NetworkInformation;
 
 namespace ConsoleDemo.Util
 {
+
     /// <summary>
     /// 图像工具
     /// </summary>
     public class ImageUtils
     {
+
+
+        private static Brush brush = new SolidBrush(Color.Black);
 
         /// <summary>
         /// 二维码Bytes转Bitmap
@@ -19,30 +23,25 @@ namespace ConsoleDemo.Util
         /// <returns>Bitmap</returns>
         public static Bitmap QrBytes2Bitmap(byte[][] bytes, int pixelSize)
         {
-            // 原二维码
-            int oldSize = bytes.Length;
-            Bitmap oldQr = new Bitmap(oldSize, oldSize);
-            for (int x = 0; x < oldSize; x++)
+            int length = bytes.Length;
+            List<Rectangle> rects = new List<Rectangle>();
+            for (int x = 0; x < length; x++)
             {
-                for (int y = 0; y < oldSize; y++)
+                for (int y = 0; y < length; y++)
                 {
                     if (bytes[x][y] == 1)
                     {
-                        oldQr.SetPixel(x, y, Color.Black);
+                        rects.Add(new Rectangle((x + 1) * pixelSize, (y + 1) * pixelSize, pixelSize, pixelSize));
                     }
                 }
             }
-            // 新二维码
-            int newSize = (oldSize + 2) * pixelSize;
-            Bitmap newQr = new Bitmap(newSize, newSize);
-            using (Graphics g = Graphics.FromImage(newQr))
+            int size = (length + 2) * pixelSize;
+            Bitmap bitmap = new Bitmap(size, size);
+            using (Graphics g = Graphics.FromImage(bitmap))
             {
-                g.FillRectangle(new SolidBrush(Color.White), new Rectangle(0, 0, newSize, newSize));
-                g.SmoothingMode = SmoothingMode.None;
-                g.InterpolationMode = InterpolationMode.NearestNeighbor;
-                g.DrawImage(oldQr, new RectangleF(10, 10, newSize - 10, newSize - 10), new Rectangle(0, 0, oldSize, oldSize), GraphicsUnit.Pixel);
+                g.FillRectangles(brush, rects.ToArray());
             }
-            return newQr;
+            return bitmap;
         }
 
         /// <summary>
